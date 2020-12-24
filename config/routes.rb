82c9c38f2e119483bcad3ae1611c6f16653f8106
Yaro <yshmarov@gmail.com>
2020-12-24 20:24:03 +0000

    rails g controller static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "static_pages#landing_page"
+  # get 'static_pages/landing_page'
+  # get 'static_pages/privacy_policy'
+  get "privacy_policy", to: "static_pages#privacy_policy"
 end
