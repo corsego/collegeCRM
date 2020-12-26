2413783c2a3738979d84d45a4fb49469842cc197
Yaro <yshmarov@gmail.com>
2020-12-26 14:38:06 +0000

    install fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,5 @@ ActiveStorage.start()
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 require("stylesheets/application.scss")
+
+import "@fortawesome/fontawesome-free/css/all"
