f6c430cb610ee1cc00233e821f44957cfc5e7bc6
Yaro <yshmarov@gmail.com>
2020-12-26 21:39:47 +0000

    create fake users with seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "admin@example.com", password: "admin@example.com", password_confirmation: "admin@example.com")
+User.create(email: "student@example.com", password: "student@example.com", password_confirmation: "student@example.com")
+User.update_all confirmed_at: DateTime.now
