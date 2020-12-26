eff09b07cac2b7be1d993288f978342f4c757d90
Yaro <yshmarov@gmail.com>
2020-12-26 20:53:27 +0000

    users index and user show pages

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all.order(created_at: :desc)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
+
+  resources :users, only: [:index, :show]
+
   root "static_pages#landing_page"
   # get 'static_pages/landing_page'
   # get 'static_pages/privacy_policy'
