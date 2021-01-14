b144a37e3667ac6fd787c99bf47f54c748ab5eeb
Yaro <yshmarov@gmail.com>
2021-01-14 12:40:18 +0000

    users can be deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: "User was successfully destroyed."
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { 
     confirmations: 'users/confirmations', 
     omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :destroy]
 
   root "static_pages#landing_page"
   # get 'static_pages/landing_page'
