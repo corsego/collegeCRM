174ee84792519079f48e63cf20e06a443cbbc036
Yaro <yshmarov@gmail.com>
2021-01-16 06:37:05 +0000

    users can be banned and unbanned

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def ban
+    @user = User.find(params[:id])
+    if @user.access_locked?
+      @user.unlock_access!
+    else
+      @user.lock_access!
+    end
+    redirect_to @user, notice: "User access locked: #{@user.access_locked?}"
+  end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,11 @@
+module UsersHelper
+  
+  def ban_status(user)
+    if user.access_locked?
+      "Unban"
+    else
+      "Ban"
+    end
+  end
+  
+end
