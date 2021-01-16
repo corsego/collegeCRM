e9d05147afe83c88534c84c5b1661ba11b500d59
Yaro <yshmarov@gmail.com>
2021-01-16 18:09:58 +0000

    styling for true false values

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def boolean_label(value)
+    case value
+      when true
+        content_tag(:span, value, class: "badge badge-success")
+      when false
+        content_tag(:span, value, class: "badge badge-danger")
+    end
+  end
+
 end
