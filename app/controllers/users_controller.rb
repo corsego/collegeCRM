879ecb9ceb14a633080a388fc7dd3437b9825987
Yaro <yshmarov@gmail.com>
2021-01-17 17:32:58 +0000

    authorization for resend_confirmation_instructions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :require_admin, only: [:edit, :update, :ban, :destroy]
+  before_action :require_admin, only: [:edit, :update, :ban, :destroy, :resend_confirmation_instructions]
 
   def index
     @users = User.all.order(created_at: :desc)
