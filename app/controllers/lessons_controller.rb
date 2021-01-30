0e81849661c77ab93e2e442f6b2b9cb04201d0bd
Yaro Shm <yshmarov@gmail.com>
2021-01-30 16:29:57 +0000

    clean up controllers from unused scaffold files

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,73 +1,48 @@
 class LessonsController < ApplicationController
   before_action :set_lesson, only: [:show, :edit, :update, :destroy]
 
-  # GET /lessons
-  # GET /lessons.json
   def index
     @lessons = Lesson.all
   end
 
-  # GET /lessons/1
-  # GET /lessons/1.json
   def show
   end
 
-  # GET /lessons/new
   def new
     @lesson = Lesson.new
   end
 
-  # GET /lessons/1/edit
   def edit
   end
 
-  # POST /lessons
-  # POST /lessons.json
   def create
     @lesson = Lesson.new(lesson_params)
 
-    respond_to do |format|
-      if @lesson.save
-        format.html { redirect_to @lesson, notice: 'Lesson was successfully created.' }
-        format.json { render :show, status: :created, location: @lesson }
-      else
-        format.html { render :new }
-        format.json { render json: @lesson.errors, status: :unprocessable_entity }
-      end
+    if @lesson.save
+      redirect_to @lesson, notice: 'Lesson was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /lessons/1
-  # PATCH/PUT /lessons/1.json
   def update
-    respond_to do |format|
-      if @lesson.update(lesson_params)
-        format.html { redirect_to @lesson, notice: 'Lesson was successfully updated.' }
-        format.json { render :show, status: :ok, location: @lesson }
-      else
-        format.html { render :edit }
-        format.json { render json: @lesson.errors, status: :unprocessable_entity }
-      end
+    if @lesson.update(lesson_params)
+      redirect_to @lesson, notice: 'Lesson was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /lessons/1
-  # DELETE /lessons/1.json
   def destroy
     @lesson.destroy
-    respond_to do |format|
-      format.html { redirect_to lessons_url, notice: 'Lesson was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to lessons_url, notice: 'Lesson was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_lesson
       @lesson = Lesson.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def lesson_params
       params.require(:lesson).permit(:user_id, :classroom_id, :course_id, :status, :start)
     end
