2f16f7972c9e9808bfbf6ad64cc4a75dec2f8300
Yaro Shm <yshmarov@gmail.com>
2021-01-31 20:34:05 +0000

    display dynamic lesson information in calendar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,16 +8,24 @@ class StaticPagesController < ApplicationController
   end
   
   def calendar
+    @users = User.order(email: :asc)
+    @classrooms = Classroom.order(name: :asc)
+    @courses = Course.order(id: :desc)
+    
     if params.has_key?(:user_id)
       lessons = Lesson.includes(:user, :classroom, :course, :attendances)
-      if params[:user_id].present?
-        lessons = lessons.where(user_id: params[:user_id])
+      @user = params[:user_id]
+      @classroom = params[:classroom_id]
+      @course = params[:course_id]
+
+      if @user.present?
+        lessons = lessons.where(user_id: @user)
       end
-      if params[:classroom_id].present?
-        lessons = lessons.where(classroom_id: params[:classroom_id])
+      if @classroom.present?
+        lessons = lessons.where(classroom_id: @classroom)
       end
-      if params[:course_id].present?
-        lessons = lessons.where(course_id: params[:course_id])
+      if @course.present?
+        lessons = lessons.where(course_id: @course)
       end
       @lessons = lessons.all
     else
